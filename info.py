77ab2ccde537b9c074ff1b9bc52b3cae5f9a80b7
Bbbino <143017552+Bbbino@users.noreply.github.com>
2023-08-26 08:44:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -72,7 +72,7 @@ LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
       # URL Shortener #
 
 URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', 'omegalinks.in')
-URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', 'https://omegalinks.in/api?api=0c4d8d9a51a4cde02dcbbc6a89bebfcc53dc8c15&url=yourdestinationlink.com&alias=CustomAlias'
+URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', 'https://omegalinks.in/api?api=0c4d8d9a51a4cde02dcbbc6a89bebfcc53dc8c15&url=yourdestinationlink.com&alias=CustomAlias')
 
      # Auto Delete For Group Message (Self Delete) #
 SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 300))
